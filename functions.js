f6dc1287a164443f5eb400bc3e64ae6b7c3a485f
Glitch (koalaknightmi-bible-quiz2-1) <none>
2020-10-08 16:52:10 +0000

    🚈🚛 Updated with Glitch

--- functions.js
+++ functions.js
@@ -1,3 +1,52 @@
+const https = require('https');
+function postData(url, data, then) {
+data = JSON.stringify(data)
+console.log(url.split("/"))
+let options = {
+  hostname: "testing-sendmail-it-works.glitch.me",
+  port: 443,
+  path: "/email",
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+let req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    console.log(d)
+    then(d)
+  })
+})
+
+req.on('error', (error) => {
+  console.error(error)
+  then(error)
+})
+
+req.write(data)
+req.end()
+  /*return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+      return resolve(xhr.responseText);
+    };
+    xhr.onerror = function () {
+      return reject(xhr.statusText);
+    };
+    xhr.send(data);
+  }).then(function (successMessage) {
+    then(successMessage);
+  }).catch(function (error) {
+    console.log("no internet doing default", error, JSON.stringify(data));
+    //location.reload();
+  });*/
+}
 exports.timeSince = function (date) {
   if (typeof date !== 'object') {
     date = new Date(date);
@@ -56,59 +105,71 @@ exports.asort = (a,t,s) => {
     }
     else if(t==="hl"&&a[i].score>ha2[a[i].ch].score){
       ha2[a[i].ch] = a[i]
+      //console.log("new best for "+a[i].ch+" score: "+a[i].score+ " type: "+a[i].type)
     }
-    else if(a[i].score<ha2[a[i].ch].score){
+    else if(t==="lh"&&a[i].score<ha2[a[i].ch].score){
       ha2[a[i].ch] = a[i]
+      //console.log("new best for "+a[i].ch+" score: "+a[i].score+ " type: "+a[i].type)
     }
   }
 //console.log(ha2)
-return ha2;
+  let ha3 = {};
+Object.keys(ha2).sort(function(a,b){
+  if (parseInt(a.replace(/[a-z]/g, ''))<parseInt(b.replace(/[a-z]/g, ''))) {
+    return -1;
+  } else {
+    return 1;
+  }
+}).forEach(function(key) {
+  ha3[key] = ha2[key];
+});
+return ha3;
 };
 exports.pad = function (l, s) {
   return (l -= this.length) > 0 ? (s = new Array(Math.ceil(l / s.length) + 1).join(s)).substr(0, s.length) + this + s.substr(0, l - s.length) : this;
 };
-exports.push = (opt,to,subs,webPush) => {
+exports.push = (opt,to,ref,webPush) => {
   if (to === "") {
-    subs.findAll().then(users => {
-      for (var i in users) {
-        console.log(users[i].dataValues.sub);
-        return webPush.sendNotification(users[i].dataValues.sub, opt).catch((err) => {
+    let query = ref.get()
+  .then(subs => { 
+    subs.forEach(sub => {
+      //console.log(sub.data().sub);
+        return webPush.sendNotification(sub.data().sub, opt).catch((err) => {
           if (err.statusCode === 410) {
-            //console.log(err)
+            console.log('Subscription is no longer valid: ', err);
+            ref.doc(sub.id).delete();
           }
           else {
-            //console.log('Subscription is no longer valid: ', err);
-            subs.destroy({
-              where: {
-                sub: users[i].dataValues.sub
-              }
-            });
+            console.log(err)
           }
         });
-      }
+      });
     });
   }
   else {
-    subs.findAll({
-      where: {
-        userName: to
-      }
-    }).then(users => {
-      for (var i in users) {
-        return webPush.sendNotification(users[i].dataValues.sub, opt).catch((err) => {
+    let query = ref.where("userName","==",to).get()
+    .then(subs => { 
+      if (subs.empty) {
+        console.log('No matching documents.');
+        return;
+      }  
+      subs.forEach(sub => {
+      //console.log(sub.data().sub);
+        return webPush.sendNotification(sub.data().sub, opt).catch((err) => {
           if (err.statusCode === 410) {
-            //console.log(err)
+            console.log('Subscription is no longer valid: ', err);
+            ref.doc(sub.id).delete();
           }
           else {
-            //console.log('Subscription is no longer valid: ', err);
-            subs.destroy({
-              where: {
-                sub: users[i].dataValues.sub
-              }
-            });
+            console.log(err)
           }
         });
-      }
+      });
     });
   }
+}
+exports.sendmail = function(to){
+  postData("https://testing-sendmail-it-works.glitch.me/email",{to:to},function(s){
+    console.log(s)
+  })
 }
